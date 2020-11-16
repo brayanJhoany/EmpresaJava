470344a1f634d4d0d1f8a4f6bfb240da6e549271
BrayanEscobar <brayan.escobar@live.com>
2020-11-16 11:02:44 -0300

    Se añade la clase estación de servicio y precios

---
+++ Empresa/src/Modelo/EstacionDeServicio.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Brayan Escobar
+ */
+public class EstacionDeServicio {
+    private int id;
+    private String direccion;
+    private String ip;
+    private int puerto;
+
+    public EstacionDeServicio(int id, String direccion, String ip, int puerto) {
+        this.id = id;
+        this.direccion = direccion;
+        this.ip = ip;
+        this.puerto = puerto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
+    
+    
+}
